ef37261246540574236b08e4bb5be07841b1255b
Siganmorfu <hu881121@gmail.com>
2020-12-15 21:34:05 +0800

    player m

--- src/draw10AtStart/Player.java
+++ src/draw10AtStart/Player.java
@@ -3,7 +3,7 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Player implements Serializable{
+public class Player implements Serializable {
     private String name;
     private String password;
 
