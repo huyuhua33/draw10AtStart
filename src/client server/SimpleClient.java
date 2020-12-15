da66a76faa422bf5df9f97bc1ea044d00a805e99
Siganmorfu <hu881121@gmail.com>
2020-12-15 23:05:58 +0800

    connection

--- src/client server/SimpleClient.java
+++ src/client server/SimpleClient.java
@@ -1,3 +1,4 @@
+
 //*******************************************************************
 //*  Network Programming - Unit 2 Simple Client and Server          *
 //*  Program Name: SimpleClient2                                     *
@@ -11,78 +12,58 @@ import java.nio.*;
 import java.util.Scanner;
 import draw10AtStart.*;
 
-
-
-public class SimpleClient
-{
-		public static byte[] intToByteArray(int value)
-	{
-		return new byte[]
-		{
-			(byte)(value >> 24),
-			(byte)(value >> 16),
-			(byte)(value >> 8),
-			(byte)value
-		};
+public class SimpleClient {
+	public static byte[] intToByteArray(int value) {
+		return new byte[] { (byte) (value >> 24), (byte) (value >> 16), (byte) (value >> 8), (byte) value };
 	}
 
-	public static int ByteToInt(byte[] buf)
-	{
-		int i,value = 0;
-		for(i = 0;i < 4;i++)
-		{
-			value = value * 256 + (buf[i] &0x7F + ((buf[i]&0x80)>>7)*128) ; 
+	public static int ByteToInt(byte[] buf) {
+		int i, value = 0;
+		for (i = 0; i < 4; i++) {
+			value = value * 256 + (buf[i] & 0x7F + ((buf[i] & 0x80) >> 7) * 128);
 		}
 		return value;
 	}
-	public static void main(String args[])
-	{
-		Socket			    client = null;
-		ObjectInputStream 	in = null;
-		ObjectOutputStream 	out = null;
-		InputStream         inmsg = null;
-		OutputStream        outmsg = null;
-		int				    port = 6666;
-		byte []			    buf = new byte[100];
-		int                 count = 0;
-		
+
+	public static void main(String args[]) {
+		Socket client = null;
+		ObjectInputStream in = null;
+		ObjectOutputStream out = null;
+		InputStream inmsg = null;
+		OutputStream outmsg = null;
+		int port = 6666;
+		byte[] buf = new byte[100];
+		int count = 0;
+
 		Scanner scanner = new Scanner(System.in);
 
-			try
-			{
-			    // Creates a stream socket and connects it to the specified port number 
-			    // at the specified IP address.
-				client = new Socket("127.0.0.1", port);
+		try {
+			// Creates a stream socket and connects it to the specified port number
+			// at the specified IP address.
+			client = new Socket("127.0.0.1", port);
 
-					// Send message to server
-					out = new ObjectOutputStream(client.getOutputStream());
-					in = new ObjectInputStream(new BufferedInputStream(client.getInputStream()));
-					
-					Player player  = new Player(null, null);
-					out.writeObject(player);
-					out.flush();
-					outmsg = client.getOutputStream();
+			// Send message to server
+			out = new ObjectOutputStream(client.getOutputStream());
+			in = new ObjectInputStream(new BufferedInputStream(client.getInputStream()));
 
-					// Read message from server
-					inmsg = client.getInputStream();
-					inmsg.read(buf);
-					System.out.println("Receive message: " + new String(buf));
-				
-				
-				
-				
-				out.close();
-				in.close();
+			Player player = new Player(null, null);
+			out.writeObject(player);
+			out.flush();
+			outmsg = client.getOutputStream();
 
-				client.close();
-			}
-			catch(UnknownHostException e)
-			{
-				e.printStackTrace();
-			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-			}
+			// Read message from server
+			inmsg = client.getInputStream();
+			inmsg.read(buf);
+			System.out.println("Receive message: " + new String(buf));
+
+			out.close();
+			in.close();
+
+			client.close();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/client server/SimpleServer.java
+++ src/client server/SimpleServer.java
@@ -1,3 +1,4 @@
+
 //*******************************************************************
 //*  Network Programming - Unit 2 Simple Client and Server          *
 //*  Program Name: SimpleServer2                                    *
@@ -6,61 +7,45 @@
 //*******************************************************************
 import java.net.*;
 import java.io.*;
-import draw10AtStart.*;
+//import draw10AtStart.*;
 
-
-public class SimpleServer
-{
-	public static byte[] intToByteArray(int value)
-	{
-		return new byte[]
-		{
-			(byte)(value >> 24),
-			(byte)(value >> 16),
-			(byte)(value >> 8),
-			(byte)value
-		};
+public class SimpleServer {
+	public static byte[] intToByteArray(int value) {
+		return new byte[] { (byte) (value >> 24), (byte) (value >> 16), (byte) (value >> 8), (byte) value };
 	}
 
-	public  static int ByteToInt(byte[] buf)
-	{
-		int i,value = 0;
-		for(i = 0;i < 4;i++)
-		{
-			value = value * 256 + (buf[i] &0x7F + ((buf[i]&0x80)>>7)*128) ; 
+	public static int ByteToInt(byte[] buf) {
+		int i, value = 0;
+		for (i = 0; i < 4; i++) {
+			value = value * 256 + (buf[i] & 0x7F + ((buf[i] & 0x80) >> 7) * 128);
 		}
 		return value;
 	}
-	public static void main(String args[])
-	{
-		ServerSocket			srverSocket = null;
-		ObjectInputStream		in = null;
-		ObjectOutputStream		out = null;
-		InputStream         	inmsg = null;
-		OutputStream        	outmsg = null;
-		byte []					buf = new byte[100];
-		Socket					sc1 = null;
-		Socket         			 sc2 = null;
- 		int						port = 6666;
 
-		
-		try
-		{
-		    // Creates a server socket, bound to the specified port.
+	public static void main(String args[]) {
+		ServerSocket srverSocket = null;
+		ObjectInputStream in = null;
+		ObjectOutputStream out = null;
+		InputStream inmsg = null;
+		OutputStream outmsg = null;
+		byte[] buf = new byte[100];
+		Socket sc1 = null;
+		Socket sc2 = null;
+		int port = 6666;
+
+		try {
+			// Creates a server socket, bound to the specified port.
 			srverSocket = new ServerSocket(port);
-			
+
 			System.out.println("Waiting for request ...");
-			try
-			{
-				while(true)
-				{
-					try
-					{
+			try {
+				while (sc2 == null) {
+					try {
 						sc1 = srverSocket.accept();
 						System.out.println("Player1 come in server!!");
-						in = new ObjectInputStream(new BufferedInputStream(sc1.getInputStream())); 
+						in = new ObjectInputStream(new BufferedInputStream(sc1.getInputStream()));
 						Object obj = in.readObject();
-						Player player1  = (Player)obj;
+						Player player1 = (Player) obj;
 						outmsg = sc1.getOutputStream();
 						String data = "Connect success\n waiting for Player2.............";
 						outmsg.write(data.getBytes());
@@ -68,8 +53,8 @@ public class SimpleServer
 						sc2 = srverSocket.accept();
 						System.out.println("Player2 come in server!!");
 						in = new ObjectInputStream(new BufferedInputStream(sc2.getInputStream()));
-						obj = in.readObject(); 
-						Player player2  = (Player)obj;
+						obj = in.readObject();
+						Player player2 = (Player) obj;
 						outmsg = sc2.getOutputStream();
 						data = "Connect success";
 						outmsg.write(data.getBytes());
@@ -77,29 +62,17 @@ public class SimpleServer
 						outmsg = sc1.getOutputStream();
 						data = "Play2 is ready ";
 						outmsg.write(data.getBytes());
-					}
-					catch (IOException e) 
-					{
-						System.err.println(e);
-					} 
-					catch(ClassNotFoundException e)
-					{
+					} catch (Exception e) {
 						System.err.println(e);
 					}
 
 				}
-			}
-			catch(IOException e)
-			{
+			} catch (Exception e) {
 				System.err.println(e);
-			}
-			finally
-			{
+			} finally {
 				srverSocket.close();
 			}
-		}
-		catch(IOException e)
-		{
+		} catch (Exception e) {
 			System.err.println(e);
 		}
 	}
